0712c9344da08ad91fcd5c3a1a637ff267e38de7
rwhiteside90 <68201063+rwhiteside90@users.noreply.github.com>
2024-01-01 18:11:29 -0500

    Create installwaveclient.sh

---
+++ installwaveclient.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+# Script to install Hanwha Wave
+#############################################################################################
+############################################
+# INSTALL DIRS
+
+HANWHADIR="/opt/hanwha/client"
+############################################
+## Exit script if not root
+if [[ `id -u` != 0 ]]; then
+    echo "Must be root to run script"
+    exit
+fi
+## Exit if not running in bash
+BASHCHECK="false"
+if [[ "${BASH_VERSINFO:-0}" -ge 4 ]]; then
+        BASHCHECK="true"
+fi
+if [ $BASHCHECK = "false" ]; then
+        echo "Script must be ran in BASH... Exiting..."
+        exit
+fi
+############################################
+
+FindLatestVersion () {
+local $NXBASEURL
+local $NXVERSION
+local $NXCURRENTVERSION
+
+NXBASEURL=`curl -s $JSON | jq '.packages_urls[]|select(. | contains("beta") | not)' | sed 's/"//g'`
+NXVERSION=`curl -s $JSON | jq '.releases[1]|select(.publication_type | startswith("release"))' | jq '.version' | sed 's/"//g'`
+if [[ "$NXVERSION" == *"4."* ]]; then
+echo "Detected legacy version 4.x... Exiting..."
+exit;
+fi
+NXCURRENTVERSION=`cat ${NXDIR}/build_info.json | jq .vmsVersion | sed 's/"//g'`
+echo "NX Current Version: $NXCURRENTVERSION"
+echo "-------------------------------"
+echo "NX Base URL: $NXBASEURL"
+echo "NX Latest Version: $NXVERSION"
+
+if [[ "$NXVERSION" == "$NXCURRENTVERSION" ]]; then
+echo "Latest version already installed... Exiting..."
+exit;
+fi
+}
+
+
+echo "Updating system using apt-get....."
+apt autoremove -y
+apt-get update -y
+apt-get install jq -y
+apt-get upgrade -y
+
+
+    NXSW="HANWHA"
+    NXDIR=$HANWHADIR
+    JSON="https://updates.vmsproxy.com/hanwha/releases.json"
+    FindLatestVersion
+    NXDEBURL="$NXBASEURL/$NXVERSION/linux/wave-client-$NXVERSION-linux_x64.deb"
+    echo "NX DEB URL: $NXDEBURL"
+
+
+
+echo "-------------------"
+echo "Downloading latest version of NX from URL: $NXDEBURL......"
+cd /tmp
+F=$(wget --content-disposition $NXDEBURL 2>&1 | grep "Saving to:")
+F2=`echo "$F" | sed 's/Saving to: ‘//g'`
+F2=`echo "$F2" | sed 's/’//g'`
+F2=`echo "$F2" | sed 's/ //g'`
+
+echo "Saved download to /tmp/$F2....."
+echo "-------------------"
+ls -lh $F2
+echo "-------------------"
+echo "Install updated installer for NX using file: $F2"
+dpkg -i $F2
+echo "-------------------"
+echo "Running apt --fix-broken install -y...."
+apt --fix-broken install -y
+echo "-------------------"
+echo "Install updated installer for NX using file: $F2"
+dpkg -i $F2
+echo "-------------------"
+echo "Removing installer files...."
+rm -fr $F2
+echo "-------------------"
