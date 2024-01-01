f3dfd9e3ce63f8e64a608fea5600c98f4efbde95
rwhiteside90 <68201063+rwhiteside90@users.noreply.github.com>
2024-01-01 18:12:36 -0500

    Create updatenxclient.sh

---
+++ updatenxclient.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+# Script to update system using apt-get and update NX Witness Server
+# Can be run directly from internet using: curl -s https://raw.githubusercontent.com/rwhiteside90/NXVMS/main/updatenx.sh | sudo bash
+# JQ is required to parse JSON FILES FROM NX WITNESS AND WILL BE INSTALLED USING APT-GET
+# Todo:
+# - API call to backup server before installing new client
+#############################################################################################
+############################################
+# INSTALL DIRS
+HANWHADIR="/opt/hanwha/client"
+DWSPECTRUMDIR="/opt/dwspectrum/client"
+############################################
+## Exit script if not root
+if [[ `id -u` != 0 ]]; then
+    echo "Must be root to run script"
+    exit
+fi
+## Exit if not running in bash
+BASHCHECK="false"
+if [[ "${BASH_VERSINFO:-0}" -ge 4 ]]; then
+        BASHCHECK="true"
+fi
+if [ $BASHCHECK = "false" ]; then
+        echo "Script must be ran in BASH... Exiting..."
+        exit
+fi
+############################################
+
+FindLatestVersion () {
+local $NXBASEURL
+local $NXVERSION
+local $NXCURRENTVERSION
+
+NXBASEURL=`curl -s $JSON | jq '.packages_urls[]|select(. | contains("beta") | not)' | sed 's/"//g'`
+NXVERSION=`curl -s $JSON | jq '.releases[1]|select(.publication_type | startswith("release"))' | jq '.version' | sed 's/"//g'`
+if [[ "$NXVERSION" == *"4."* ]]; then
+echo "Detected legacy version 4.x... Exiting..."
+exit;
+fi
+NXCURRENTVERSION=`cat ${NXDIR}/build_info.json | jq .vmsVersion | sed 's/"//g'`
+echo "NX Current Version: $NXCURRENTVERSION"
+echo "-------------------------------"
+echo "NX Base URL: $NXBASEURL"
+echo "NX Latest Version: $NXVERSION"
+
+if [[ "$NXVERSION" == "$NXCURRENTVERSION" ]]; then
+echo "Latest version already installed... Exiting..."
+exit;
+fi
+}
+
+
+echo "Updating system using apt-get....."
+apt autoremove -y
+apt-get update -y
+apt-get install jq -y
+apt-get upgrade -y
+
+if [ -d "$HANWHADIR" ];
+then
+    NXSW="HANWHA"
+    NXDIR=$HANWHADIR
+    JSON="https://updates.vmsproxy.com/hanwha/releases.json"
+    FindLatestVersion
+    NXDEBURL="$NXBASEURL/$NXVERSION/linux/wave-client-$NXVERSION-linux_x64.deb"
+    echo "NX DEB URL: $NXDEBURL"
+elif [ -d "$DWSPECTRUMDIR" ];
+then
+    NXSW="DWSPECTRUM"
+    NXDIR=$DWSPECTRUMDIR
+    JSON="https://updates.vmsproxy.com/digitalwatchdog/releases.json"
+    FindLatestVersion
+    NXDEBURL="$NXBASEURL/$NXVERSION/linux/dwspectrum-client-$NXVERSION-linux_x64.deb"
+    echo "NX DEB URL: $NXDEBURL"
+else
+    echo "No NXVMS software detected... Exiting..."
+    exit;
+fi
+
+
+echo "-------------------"
+echo "Downloading latest version of NX from URL: $NXDEBURL......"
+cd /tmp
+F=$(wget --content-disposition $NXDEBURL 2>&1 | grep "Saving to:")
+F2=`echo "$F" | sed 's/Saving to: ‘//g'`
+F2=`echo "$F2" | sed 's/’//g'`
+F2=`echo "$F2" | sed 's/ //g'`
+
+echo "Saved download to /tmp/$F2....."
+echo "-------------------"
+ls -lh $F2
+echo "-------------------"
+echo "Install updated installer for NX Client using file: $F2"
+dpkg -i $F2
+echo "-------------------"
+echo "Removing installer files...."
+rm -fr $F2
