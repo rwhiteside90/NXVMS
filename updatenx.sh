50d84381ed32a2dc8b69dd23d12f47278dbba45b
rwhiteside90 <68201063+rwhiteside90@users.noreply.github.com>
2024-01-01 19:43:56 -0500

    Update updatenx.sh

--- updatenx.sh
+++ updatenx.sh
@@ -8,7 +8,7 @@
 # Added DEBIAN_FRONTEND=noninteractive
 # Added logic to prevent installing older version than what's installed
 # Fixed query to select latest version from JSON
-# Added second for DW Spectrum has it's renamed in newer versions
+# Added second for DW Spectrum has it's renamed in newer versions (dwspectrum vs digitalwatchdog)
 #############################################################################################
 export DEBIAN_FRONTEND=noninteractive
 ############################################
